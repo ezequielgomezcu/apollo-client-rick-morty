64bb37d628c377c49f83da7c75d33918bbc175a6
Ezequiel Gómez <ezequiel.gomez@softonic.com>
2020-02-24 11:59:07 +0100

    apollo client created and first query made

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql } from 'apollo-boost';
+
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
+
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        characters(name: "Rick", gender: "", status: "") {
+          id name gender status image
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
