ccecbab9ead6cd55ffdfdc406f2c85f7c4aa9ffc
Ezequiel Gómez <ezequiel.gomez@softonic.com>
2020-02-24 14:44:33 +0100

    InMemoryCache implemented

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
@@ -10,6 +11,7 @@ import App from './App';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/',
+  cache: new InMemoryCache(),
   connectToDevTools: true,
 });
 
